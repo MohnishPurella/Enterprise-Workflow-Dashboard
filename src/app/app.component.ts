6a00b699d4f54594c2e7cab82579ba7d5272591f
MohnishPurella <purella1597@gmail.com>
2025-03-25 01:44:19 +0530

    implemented primng

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { AccordionModule } from 'primeng/accordion';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ButtonModule, AccordionModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
