9ed3720f554ba18644808f0f950595aa1f17be02
MohnishPurella <purella1597@gmail.com>
2025-04-20 03:43:45 +0530

    reseted files

--- src/app/Pipes/Task Proprity/task-priority.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { TaskPriorityPipe } from './task-priority.pipe';
-
-describe('TaskPriorityPipe', () => {
-  it('create an instance', () => {
-    const pipe = new TaskPriorityPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
--- src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { TaskListComponent } from './Components/Task/task-list/task-list.component';
-import { TaskDetailsComponent } from './Components/Task/task-details/task-details.component';
-
-export const routes: Routes = [
-    { path:'tasks', component:TaskListComponent },
-    { path: 'tasks/details/:id', component: TaskDetailsComponent },
-];
