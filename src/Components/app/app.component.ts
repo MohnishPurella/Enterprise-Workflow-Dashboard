0f9ceca657a0f092358216bb429f03c084b71a68
MohnishPurella <purella1597@gmail.com>
2025-04-20 16:23:33 +0530

    implemented navbar

--- src/Components/app/app.component.ts
+++ src/Components/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
+import { HeaderComponent } from '../header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ButtonModule],
+  imports: [RouterOutlet, ButtonModule, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
